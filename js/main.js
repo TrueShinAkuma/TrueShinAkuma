671ac666c70bbb2af6f54529fc85f4000ad89d1d
Shin Akuma <shinakuma.work@gmail.com>
2020-09-11 17:45:11 +0500

    Jetro

--- js/main.js
+++ js/main.js
@@ -1,47 +1,24 @@
-$(function(){
-    
-    
-    $('.slider__inner').slick({
-      arrows: false,
-      dots: true,
-    });
-
-    $('.works__inner').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.svg" alt=""></button>',
-      nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.svg" alt=""></button>',
-      responsive: [
-        {
-          breakpoint: 911 ,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-          breakpoint: 681 ,
-          settings: {
-            slidesToShow: 2,
-          }
-        },
-        {
-          breakpoint: 455 ,
-          settings: {
-            slidesToShow: 1,
-          }
-        },
-      ]
-    });
-
-    $('.header__menu-btn').on('click', function(){
-      $('.header__menu > ul').slideToggle();
-    });
-
-    $('.header__drop-down.drop-down').on('click', function(){
-      $(this).children('.drop-down__list').toggleClass('active');
-      $(this).children('.drop-down__link').toggleClass('active');
-    });
-
-
-
+$(function () {
+  $(".slider__inner").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<span class="slick-arrow slick-prev"></span>',
+    nextArrow: '<span class="slick-arrow slick-next"></span>',
+    fade: true,
+    asNavFor: ".slider__nav",
   });
+  $(".slider__nav").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: ".slider__inner",
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    variableWidth: true,
+  });
+
+  $(".header__nav-btn").on("click", function () {
+    $(".menu").slideToggle();
+  });
+});
